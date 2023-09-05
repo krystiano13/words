83d63fda8352b6a77b4405133ffbfbeba42661da
krystiano13 <crystianotv@gmail.com>
2023-09-05 17:47:21 +0200

    create User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $fields = $request -> validate([
+            'name' => ['required', 'unique:users'],
+            'email' => ['email', 'required', 'unique:users'],
+            'password' => ['required','min:8','confirmed'],
+        ]);
+
+        $user = User::create($fields);
+
+        auth() -> login($user);
+
+        return redirect('/');
+    }
+
+    public function login(Request $request) {
+        $fields = $request -> validate([
+            'name' => ['required'],
+            'password' => ['required']
+        ]);
+
+        if(auth() -> attempt(['name' => $fields['name'], 'password' => $fields['password']])) {
+            return redirect('/');
+        }
+    }
+
+    public function logout() {
+        auth() -> logout();
+        return redirect('/');
+    }
 }
